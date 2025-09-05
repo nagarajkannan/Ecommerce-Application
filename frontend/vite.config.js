7190781cafc7133759cdb5774aa5fc75c3258ad7
Nagaraj <nagarajkannan2003@gmail.com>
2025-09-05 15:25:54 +0530

    Connect frontend to backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // allows 0.0.0.0
+    host: true,
     port: 4173,
-    strictPort: false,
-    preview: {
-      allowedHosts: [
-        'ecommerce-application-2-162t.onrender.com'
-      ]
-    }
+    strictPort: true,
   },
   preview: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 4173,
-    strictPort: false,
     allowedHosts: [
-      'ecommerce-application-2-162t.onrender.com'
+      "ecommerce-application-2-162t.onrender.com"
     ]
-  }
+  },
+  base: "./",
 });
